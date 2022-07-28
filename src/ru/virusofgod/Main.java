9d509e2c262f98e109cc8212e8ef385518cf8266
virusofgod <virusofgod@yandex.ru>
2022-07-28 14:48:58 +0400

    Lesson 15 part 2 example 1 logical operation

--- src/ru/virusofgod/Main.java
+++ src/ru/virusofgod/Main.java
@@ -1,6 +1,28 @@
 package ru.virusofgod;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+      //logical operation
+        /* {
+            Scanner in = new Scanner(System.in);
+            if (in.nextInt() > 10) { // шпаргалка (== , > , <, >= , <= , && , || , != ,true , false).
+                System.out.println("Условие сгенерировано true");
+            } else {
+                System.out.println("Условие сгенерировано false");
+            }
+        }*/
+      /*  {
+            System.out.print("введите x: ");
+            Scanner in = new Scanner(System.in);
+            in.useLocale(Locale.US);
+            double x = in.nextInt();
+            boolean a = !(x < 5 || x > 5 && x < 7);
+            System.out.println("----------------------------");
+            System.out.println(a);
+        }*/
     }
 }
